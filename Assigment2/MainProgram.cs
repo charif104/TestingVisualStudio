4152a8035bde79d7d97baf387af50609837aaf13
charif104 <charif.ramadan@hotmail.com>
2019-11-12 12:52:15 +0100

    startign with c#

---
+++ Assigment2/MainProgram.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assigment2
+{
+    class MainProgram
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
